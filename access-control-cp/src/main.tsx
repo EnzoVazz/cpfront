43e81b2d15ff9080df415e7c1288fa0fafef4643
EnzoVazz <evaz0711@gmail.com>
2025-10-04 16:43:11 -0300

    feat - Criando os endereços das rotas

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -2,11 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Erro from './routes/Erro/index.tsx';
+import Login from './routes/Login/index.tsx';
+import Cadastro from './routes/Cadastro/index.tsx';
 
 const router = createBrowserRouter([
   {
-    
-  }
+    path: "/",
+    element: <App/>, 
+    errorElement:<Erro/>,
+    children:[
+      {
+        path:"/",
+        element:<Login/>,
+      },
+      {
+        path:"/Login",
+        element:<Login/>,
+      },
+      {
+        path:"/Cadastro",
+        element:<Cadastro/>
+      },
+    ],
+  },
 ]);
 
 
