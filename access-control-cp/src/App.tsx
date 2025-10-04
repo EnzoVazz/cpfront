896c483b9bf460931853f0e17895d5a8605cebf1
LucasFukuda2408 <luquinhasryuji@gmail.com>
2025-10-04 20:28:44 -0300

    feat - Adicionando função de logout, usando UseEffect para carregar os dados do usuário.

--- access-control-cp/src/App.tsx
+++ access-control-cp/src/App.tsx
@@ -1,4 +1,5 @@
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate, Link } from "react-router-dom";
 
 interface TipoUsuario{
   id: number;
@@ -8,12 +9,41 @@ interface TipoUsuario{
 }
 
 export default function App(){
+  const [usuario, setUsuario] = useState<TipoUsuario | null>(null);
+  const navigate = useNavigate();
 
-  
+  useEffect(() => {
+    const usuarioLogado = sessionStorage.getItem('usuarioLogado');
+    if (usuarioLogado) {
+      setUsuario(JSON.parse(usuarioLogado));
+    }
+  }, []);
 
-  
+    const handleLogout = () => {
+    sessionStorage.removeItem('usuarioLogado');
+    setUsuario(null);
+    alert("Você foi desconectado.");
+    navigate('/login');
+  };
+
   return(
     <div>
+      <header>
+        <h1>Controle de acessos</h1>
+        {usuario ?(
+          <div>
+            <div>
+              <p>Bem-vindo, {usuario.nome}</p>
+              <p>{usuario.email}</p>
+            </div>
+            <button onClick={handleLogout}>Sair</button>
+          </div>
+        ) : (
+          <nav>
+            <Link to="/login">Login</Link>
+          </nav>
+        )}
+      </header>
       <main>
         <Outlet/>
       </main>
