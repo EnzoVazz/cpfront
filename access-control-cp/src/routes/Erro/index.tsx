8903e64a12b39668ce2fb3c28b2af9560bbb36ca
EnzoVazz <evaz0711@gmail.com>
2025-10-04 16:21:35 -0300

    feat - Criação da pasta routes, criando a pagina de erro.

---
+++ access-control-cp/src/routes/Erro/index.tsx
@@ -0,0 +1,15 @@
+export default function Erro() {
+    return (
+        <main>
+            <section className="bg-cc-cinza h-[100vh] w-[100vw] fixed top-0 right-0">
+                <div className="flex flex-col items-center min-w-[300px] w-[80vw] max-w-[1400px] min-h-[80vh] my-[2vh] bg-white rounded-lg mx-auto mt-[14vh] px-4">
+                    
+                    <p>Nenhuma página com esse nome foi encontrada... Tente Novamente</p>
+                </div>
+
+            </section>
+          
+        </main>
+    )
+}
+
