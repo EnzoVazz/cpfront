a3b262db44a02552c1878d86af4f6196aefdfb11
LucasFukuda2408 <luquinhasryuji@gmail.com>
2025-10-04 20:05:41 -0300

    feat - Criando uma interface para TipoUsuario

--- access-control-cp/src/App.tsx
+++ access-control-cp/src/App.tsx
@@ -1,6 +1,16 @@
 import { Outlet } from "react-router-dom";
 
+interface TipoUsuario{
+  id: number;
+  nome: string;
+  nomeUsuario: string;
+  email: string;
+}
+
 export default function App(){
+
+  
+
   
   return(
     <div>
