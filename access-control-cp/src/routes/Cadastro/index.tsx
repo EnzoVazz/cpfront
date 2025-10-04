2a6c7a8cb521305402f1db7bfefd6fbce70e7ecd
EnzoVazz <evaz0711@gmail.com>
2025-10-04 17:55:18 -0300

    feat - Criando a função onSubmit para que os dados sejam capturados no formulário

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -1,10 +1,27 @@
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import type { TipoCadastro } from '../../types/TipoCadastro';
 
 export default function Cadastro() {
+  const{
+    register,
+    handleSubmit,
+    formState:{ errors },
+  } = useForm<TipoCadastro>();
+
+  const onSubmit = (data: TipoCadastro) => {
+    
+  };
+
+  
+
   return (
     <div>
       <h1>Página de Cadastro</h1>
+
+      <p>
        <Link to="/login">Já tem uma conta? Faça o login</Link>
+      </p>
     </div>
   );
 }
