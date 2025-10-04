d6700e34ee1905efce86932095a6d7cade9d92d5
EnzoVazz <evaz0711@gmail.com>
2025-10-04 18:06:41 -0300

    feat - Adicionando form para o campo email, com mensagem de erro

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -25,7 +25,21 @@ export default function Cadastro() {
                 <input {...register("nomeUsuario", { required: "O nome de usuário é obrigatório" })} placeholder="Nome de Usuário" />
                 {errors.nomeUsuario && <small>{errors.nomeUsuario.message}</small>}
             </div>
-            
+            <div>
+                <input 
+                  type="email" 
+                  {...register("email", { 
+                    required: "O e-mail é obrigatório", 
+                    pattern: { 
+                      value: /^\S+@\S+$/i, 
+                      message: "Formato de e-mail inválido" 
+                    } 
+                  })} 
+                  placeholder="E-mail" 
+                />
+                {errors.email && <small>{errors.email.message}</small>}
+            </div>
+            <button type="submit">Cadastrar</button>
       </form>
       <p>
        <Link to="/login">Já tem uma conta? Faça o login</Link>
